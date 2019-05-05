ea5f1d75475ba9a238db651e586aa205cac210ef
Madhusmita Kundo <mskundo@gmail.com>
2019-05-05 14:54:09 +0530

    Update functionality added for user

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from "axios"
-import GetUser from './GetUser'
 
 class AddUser extends React.Component {
     constructor(props) {
@@ -10,9 +9,15 @@ class AddUser extends React.Component {
             lastName: '',
             empId: '',
             addData: [],
-            getData: []
+            getData: [],
+            flag: false,
+            updateId: 0,
+            updateFirstName: '',
+            updateLastName: '',
+            updateEmpId: ''
         };
     }
+
 
     onFirstChange(e) {
         this.setState({ firstName: e.target.value })
@@ -62,64 +67,150 @@ class AddUser extends React.Component {
             });
     }
 
+    renderUser() {
+        return (
+            this.state.getData.map(data =>
+                <tbody>
+                    <tr>
+                        <td key={data.userId} className='jumbotron'>{data.firstName}</td>
+                        <button type="button" className="btn btn-primary" onClick={this.updateUser.bind(this, data.userId, data.firstName, data.lastName, data.empId)}>UPDATE</button>
+                    </tr>
+                    <tr>
+                        <td key={data.lastName} className='jumbotron'>{data.lastName}</td>
+                        <button type="button" className="btn btn-danger">DELETE</button>
+                    </tr>
+                    <tr>
+                        <td key={data.empId} className='jumbotron'>{data.empId}</td>
+                    </tr><br />
+                </tbody>
+            )
+        )
+    }
+
+    updateUser(id, fname, lname, employeeId) {
+        this.state.updateId = id
+        this.state.updateFirstName = fname
+        this.state.updateLastName = lname
+        this.state.updateEmpId = employeeId
+        this.state.flag = true
+        this.render()
+    }
+
     render() {
+        console.log(this.state.flag)
         return (
             <div className='container-fluid'><br />
-                <form className='form-group' id="userForm" onSubmit={this.onAddUser.bind(this)}>
-                    <div className='row'>
-                        <div className='col-md-2'></div>
-                        <div className='col-md-2'>
-                            <label>First Name :</label>
-                        </div>
-                        <div className='col-md-4'>
-                            <input type='text' className="form-control" name='firstName' value={this.state.firstName}
+                {this.state.flag === false ?
+                    <form className='form-group' id="userForm" onSubmit={this.onAddUser.bind(this)}>
+                        <div className='row'>
+                            <div className='col-md-2'></div>
+                            <div className='col-md-2'>
+                                <label>First Name :</label>
+                            </div>
+                            <div className='col-md-4'><input type='text' className="form-control" name='firstName' value={this.state.firstName}
                                 onChange={this.onFirstChange.bind(this)} />
-                        </div>
-                        <div className='col-md-4'></div>
-                    </div><br />
-                    <div></div>
-                    <div className='row'>
-                        <div className='col-md-2'></div>
-                        <div className='col-md-2'>
-                            <label>Last Name :</label>
-                        </div>
-                        <div className='col-md-4'>
-                            <input type='text' className="form-control" name='lastName' value={this.state.lastName}
-                                onChange={this.onLastChange.bind(this)} />
-                        </div>
-                        <div className='col-md-4'></div>
-                    </div><br />
-                    <div className='row'>
-                        <div className='col-md-2'></div>
-                        <div className='col-md-2'>
-                            <label>Employee ID :</label>
-                        </div>
-                        <div className='col-md-2'>
-                            <input type='text' className="form-control" name='lastName' value={this.state.empId}
-                                onChange={this.onEmpId.bind(this)} />
-                        </div>
-                        <div className='col-md-6'></div>
-                    </div><br />
-                    <div className='row'>
-                        <div className='col-md-7'></div>
-                        <div className='col-md-4'>
-                            <div className='row'>
-                                <div className='col-md-1'>
-                                    <button type='submit' className='btn btn-secondary'>Add</button>
-                                </div>
-                                <div className='col-md-4'>
-                                    <button type='button' className='btn btn-secondary' onClick={this.cancelCourse.bind(this)} value="Reset">Reset</button>
+                            </div>
+                            <div className='col-md-4'></div>
+                        </div><br />
+                        <div></div>
+                        <div className='row'>
+                            <div className='col-md-2'></div>
+                            <div className='col-md-2'>
+                                <label>Last Name :</label>
+                            </div>
+                            <div className='col-md-4'>
+                                <input type='text' className="form-control" name='lastName' value={this.state.lastName}
+                                    onChange={this.onLastChange.bind(this)} />
+                            </div>
+                            <div className='col-md-4'></div>
+                        </div><br />
+                        <div className='row'>
+                            <div className='col-md-2'></div>
+                            <div className='col-md-2'>
+                                <label>Employee ID :</label>
+                            </div>
+                            <div className='col-md-2'>
+                                <input type='text' className="form-control" name='lastName' value={this.state.empId}
+                                    onChange={this.onEmpId.bind(this)} />
+                            </div>
+                            <div className='col-md-6'></div>
+                        </div><br />
+                        <div className='row'>
+                            <div className='col-md-7'></div>
+                            <div className='col-md-4'>
+                                <div className='row'>
+                                    <div className='col-md-1'>
+                                        <button type='submit' className='btn btn-secondary'>Add</button>
+                                    </div>
+                                    <div className='col-md-4'>
+                                        <button type='button' className='btn btn-secondary' onClick={this.cancelCourse.bind(this)} value="Reset">Reset</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <br />
-                </form>
+                        <br />
+                        <div className="row">
+                            <table className="table table-borderless table-condensed">
+                                {this.renderUser()}
+                            </table>
+                        </div>
+                    </form>
+                    :
+                    < form className='form-group'>
+                        <div className='row'>
+                            <div className='col-md-2'></div>
+                            <div className='col-md-2'>
+                                <label>{this.state.updateFirstName}</label>
+                            </div>
+                            <div className='col-md-4'>
+                                <input type='text' className="form-control" name='firstName' value={this.state.updateFirstName}
+                                />
 
-                <div className="row">
-                    <GetUser userData={this.state.getData} />
-                </div>
+                            </div>
+                            <div className='col-md-4'></div>
+                        </div> <br />
+                        <div></div>
+                        <div className='row'>
+                            <div className='col-md-2'></div>
+                            <div className='col-md-2'>
+                                <label>Last Name :</label>
+                            </div>
+                            <div className='col-md-4'>
+                                <input type='text' className="form-control" name='lastName' value={this.state.updateLastName}
+                                />
+                            </div>
+                            <div className='col-md-4'></div>
+                        </div> <br />
+                        <div className='row'>
+                            <div className='col-md-2'></div>
+                            <div className='col-md-2'>
+                                <label>Employee ID :</label>
+                            </div>
+                            <div className='col-md-2'>
+                                <input type='text' className="form-control" name='lastName' value={this.state.updateEmpId}
+                                />
+                            </div>
+                            <div className='col-md-6'></div>
+                        </div> <br />
+                        <div className='row'>
+                            <div className='col-md-7'></div>
+                            <div className='col-md-4'>
+                                <div className='row'>
+                                    <div className='col-md-1'>
+                                        <button type='submit' className='btn btn-secondary'>Update</button>
+                                    </div>
+                                    <div className='col-md-4'>
+                                        <button type='button' className='btn btn-secondary' value="Reset">Reset</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <br />
+                    </form>
+                }
             </div>
+
+
         )
     }
 
